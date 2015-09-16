404d9164f904d27976bc4c789bb7569e0e9f3df5
Dmitry Zuev <mail@dzuev.ru>
2015-09-16 20:52:49 +0300

    Rubocop passing

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,17 +1,15 @@
+# Simple product model. Products has name, description and photo
 class Product < ActiveRecord::Base
   belongs_to :user
 
-  validates :name, presence: true, length: {maximum: 42}
+  validates :name, presence: true, length: { maximum: 42 }
   validates :photo, presence: true, on: :create
   validates :user, presence: true
 
   has_attached_file :photo, styles: { medium: '450x450>', thumb: '100x100#' },
-                        default_url: '/images/:style/missing.png',
-                        url: '/images/:hash.:extension',
-                        hash_secret: 'da9a3c0d2aaf25d6bb627051fd2cf9a4'
-  validates_attachment_content_type :photo, content_type: /\Aimage\/.*\Z/
+                            default_url: '/images/:style/missing.png',
+                            url: '/images/:hash.:extension',
+                            hash_secret: 'da9a3c0d2aaf25d6bb627051fd2cf9a4'
+  validates_attachment_content_type :photo, content_type: %r{/\Aimage\/.*\Z/}
   validates_attachment_size :photo, less_than: 4.megabytes
-
-  
-
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
+# User model with devise authentication
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
