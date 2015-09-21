65d0e2f96c9e6e4180c0cdd63fe895addd75a55f
Dmitry Zuev <mail@dzuev.ru>
2015-09-21 19:26:40 +0300

    4th task initial

--- app/jobs/place_order_job.rb
+++ app/jobs/place_order_job.rb
@@ -2,17 +2,34 @@
 class PlaceOrderJob < ActiveJob::Base
   queue_as :default
 
-  def perform(user, product)
+  rescue_from(RuntimeError) do |error|
+    retry_job wait: 5.minutes
+  end
+
+  def perform(user, product, attempt = 1)
+    delay = rand 0.0..6.0
+
+    sleep 3 && fail 'get_error' if rand > 3
+    sleep delay
     photo = retrieve_photo
     photo_file = retrieve_photo_file photo
-    todo_id = retrieve_todo
-
+    
     UserMailer.order_placed_email(user, product, photo_file)
-      .deliver_now if photo_ok? photo
-    AdminMailer.order_placed_email(todo_id)
       .deliver_now if photo_ok? photo
     AdminMailer.order_failed_email(user)
       .deliver_now unless photo_ok? photo
+
+    3.times do |i|
+      delay = rand 3.0..6.0
+      fail 'post_error' if delay > 3 && i == 2
+      break unless delay > 3
+    end
+
+    sleep delay
+    todo_id = retrieve_todo
+
+    AdminMailer.order_placed_email(todo_id)
+      .deliver_now if photo_ok? photo
   end
 
   def retrieve_photo
